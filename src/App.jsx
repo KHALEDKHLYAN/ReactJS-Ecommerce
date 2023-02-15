ce3295579e7ac3a73ee5c2b2b4aafa0d88d7d812
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-02-15 23:15:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  let title = "myname";
+  return (
+    <div>
+    <h1>Hello Khaled!!!</h1>
+    <h2>{title}</h2>
+    </div>
+  )
+}
+
+export default App
